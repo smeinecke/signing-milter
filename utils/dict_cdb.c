de01826a8810d01421739dc23384174f100522bf
Stefan Meinecke <meinecke@greensec.de>
2026-08-06 22:50:09 +0200

    Remove GPL license headers from all source files and rename COPYING to LICENSE
    - Strip GPL boilerplate from all .c and .h files
    - Rename COPYING to LICENSE for standard naming convention
    - Clean up .gitignore (remove .private.key entry and trailing newline)

--- utils/dict_cdb.c
+++ utils/dict_cdb.c
@@ -1,31 +1,3 @@
-/*
- * signing-milter - utils/dict_cdb.c
- * Copyright (C) 2010,2011  Andreas Schulze
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; only version 2 of the License is applicable.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Authors:
- *      Andreas Schulze <signing-milter at andreasschulze.de> based on
- *      postfix/src/utils/dict_cdb.c by
- *      Michael Tokarev <mjt@tls.msk.ru> based on dict_db.c by
- *      Wietse Venema
- *      IBM T.J. Watson Research
- *      P.O. Box 704
- *      Yorktown Heights, NY 10598, USA
- *
- */
-
 #include "dict_cdb.h"
 
 #include <pthread.h>
--- utils/lowercase.c
+++ utils/lowercase.c
@@ -1,30 +1,3 @@
-/*
- * signing-milter - utils/lowercase.c
- * Copyright (C) 2011-2018  Andreas Schulze
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; only version 2 of the License is applicable.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Authors:
- *      Andreas Schulze <signing-milter at andreasschulze.de> based on
- *      postfix/src/utils/{lowercase.c,sys_defs.h} by
- *      Wietse Venema
- *      IBM T.J. Watson Research
- *      P.O. Box 704
- *      Yorktown Heights, NY 10598, USA
- *
- */
-
 #include "lowercase.h"
 
 char *lowercase(char *string)
